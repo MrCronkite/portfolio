5bd28d0c1b60146a28491df0d3d1e5b0049e3796
MrCronkite <vladshima99@gmail.com>
2019-03-30 22:50:51 +0300

    worked func

--- main_site/src/App.js
+++ main_site/src/App.js
@@ -62,12 +62,14 @@ class App extends Component {
           </div>
         </div>
         <div className="myWorks">
-        <div className="bd-example">
+        <div className="content-carousel">
           <div id="carouselExampleCaptions" className="carousel slide" data-ride="carousel">
           <ol className="carousel-indicators">
             <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
             <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
             <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="4"></li>
           </ol>
           <div className="carousel-inner">
       <div className="carousel-item active">
@@ -82,6 +84,20 @@ class App extends Component {
         <div class="carousel-caption d-none d-md-block">
           <h5>Second slide label</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+      <div className="carousel-item">
+        <img src="./img/viar.png" className="d-block w-100" alt="nothing"/>
+        <div className="carousel-caption d-none d-md-block">
+          <h5>Third slide label</h5>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </div>
+      </div>
+      <div className="carousel-item">
+        <img src="./img/viar.png" className="d-block w-100" alt="nothing"/>
+        <div className="carousel-caption d-none d-md-block">
+          <h5>Third slide label</h5>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
         </div>
       </div>
       <div className="carousel-item">
