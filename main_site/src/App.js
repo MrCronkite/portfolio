b8fc7141d7dac0054575d3f1f02ff61c50dcdb1b
MrCronkite <vladshima99@gmail.com>
2019-04-03 00:11:34 +0300

    deploy main site

--- main_site/src/App.js
+++ main_site/src/App.js
@@ -13,19 +13,19 @@ class App extends Component {
       <h1>I <i id="heart" className="fa fa-heart"></i> JavaScript</h1>
       <ul className="nav justify-content-center">
   <li className="nav-item">
-    <a className="nav-link" href="#">about me</a>
+    <a className="nav-link" href="#about_me">about me</a>
   </li>
   <li className="nav-item">
-    <a className="nav-link" href="#">skills</a>
+    <a className="nav-link" href="#skills">skills</a>
   </li>
   <li className="nav-item">
-    <a className="nav-link" href="#">my works</a>
+    <a className="nav-link" href="#works">my works</a>
   </li>
 </ul>
    <h4>My love to work helps me to do it well, with attention to detail.</h4>
    <button type="button" className="btn btn-warning down_button"><i className="fa fa-arrow-circle-o-down"></i></button>
 </div>
-        <div className = "about_me">
+        <div id="about_me" className = "about_me">
           <div className="cointainer">
           <div className="row justify-content-md-center">
     <div className="col-md-auto text">
@@ -37,12 +37,12 @@ class App extends Component {
        <p> I constantly trying to develop myself. That is huge plus in our field. I am non-conflict person with high empathy level. Joining the team is always easy to me.</p>
     </div>
     <div className="col-md-auto">
-      <img src="./img/logo.jpg"/>
+      <img src="./img/logo.jpg" alt=""/>
     </div>
   </div>
           </div>
         </div>
-        <div className = "skills">
+        <div id="skills" className = "skills">
           <h2>Skills</h2>
           <h3>HTML/CSS</h3>
           <div className="progress">
@@ -61,7 +61,7 @@ class App extends Component {
             <div className="progress-bar bg-warning" role="progressbar" style={{width: "40%"}} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">40%</div>
           </div>
         </div>
-        <div className="myWorks">
+        <div id="works" className="myWorks">
         <div className="content-carousel">
           <div id="carouselExampleCaptions" className="carousel slide" data-ride="carousel">
           <ol className="carousel-indicators">
@@ -75,41 +75,41 @@ class App extends Component {
       <div className="carousel-item active">
         <img  src="./img/less.png" className="d-block w-100" alt="nothing"/>
         <div className="carousel-caption d-none d-md-block">
-        <a  href="http://economic-money.surge.sh/">check project</a>
+        <a  href="https://github.com/MrCronkite/portfolio/blob/master/README.md#less">check LESS project</a>
         </div>
       </div>
       <div className="carousel-item">
         <img src="./img/sass.png" className="d-block w-100" alt="nothing"/>
         <div className="carousel-caption d-none d-md-block">
-        <a  href="http://economic-money.surge.sh/">check project</a>
+        <a  href="https://github.com/MrCronkite/portfolio/blob/master/README.md#sass">check SASS project</a>
+        </div>
+      </div>
+      <div className="carousel-item">
+        <img src="./img/react.png" className="d-block w-100" alt="nothing"/>
+        <div className="carousel-caption d-none d-md-block">
+        <a  href="https://github.com/MrCronkite/portfolio/blob/master/README.md#reactjs">check REACT project</a>
         </div>
       </div>
       <div className="carousel-item">
         <img src="./img/viar.png" className="d-block w-100" alt="nothing"/>
         <div className="carousel-caption d-none d-md-block">
-        <a  href="http://economic-money.surge.sh/">check project</a>
+        <a  href="https://github.com/MrCronkite/portfolio/blob/master/README.md#htmlcss">check HTML/CSS project</a>
         </div>
       </div>
       <div className="carousel-item">
-        <img src="./img/viar.png" className="d-block w-100" alt="nothing"/>
+        <img src="./img/transit.png" className="d-block w-100" alt="nothing"/>
         <div className="carousel-caption d-none d-md-block">
-        <a  href="http://economic-money.surge.sh/">check project</a>
-        </div>
-      </div>
-      <div className="carousel-item">
-        <img src="./img/viar.png" className="d-block w-100" alt="nothing"/>
-        <div className="carousel-caption d-none d-md-block">
-         <a  href="http://economic-money.surge.sh/">check project</a>
+         <a  href="https://github.com/MrCronkite/portfolio/blob/master/README.md#html5css3">check HTML5/CSS33 project</a>
         </div>
       </div>
     </div>
-    <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span className="sr-only">Previous</span>
+    <a className="carousel-control-prev " href="#carouselExampleCaptions" role="button" data-slide="prev">
+      <span className="carousel-control-prev-icon button__navs" aria-hidden="true"></span>
+      <span className="sr-only button__navs">Previous</span>
     </a>
-    <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-      <span className="carousel-control-next-icon" aria-hidden="true"></span>
-      <span className="sr-only">Next</span>
+    <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-sanlide="next">
+      <span className="carousel-control-next-icon button__navs" aria-hidden="true"></span>
+      <span className="sr-only button__navs">Next</span>
     </a>
           </div>
           </div>
