d3951fc3298628a39062ba2c2c370b454b42e5f1
MrCronkite <vladshima99@gmail.com>
2019-04-04 23:46:53 +0300

    refactoring

--- main_site/src/App.js
+++ main_site/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import Slider from "react-slick"
 import './App.css';
+
 
 
 
@@ -7,6 +9,13 @@ class App extends Component {
 
 
   render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
     return (
       <div className="App">
       <div className="header">
@@ -73,7 +82,7 @@ class App extends Component {
           </ol>
           <div className="carousel-inner">
       <div className="carousel-item active">
-        <img  src="./img/less.png" className="d-block w-100" alt="nothing"/>
+        <img  src="./img/less.png" width="600px" alt="nothing"/>
         <div className="carousel-caption d-none d-md-block">
         <a  href="https://github.com/MrCronkite/portfolio/blob/master/README.md#less">check LESS project</a>
         </div>
@@ -114,6 +123,30 @@ class App extends Component {
           </div>
           </div>
         </div>
+
+        <div>
+        <h2> Single Item</h2>
+        <Slider {...settings}>
+          <div>
+            <h3>  <img  src="./img/less.png" width="600px" alt="nothing"/></h3>
+          </div>
+          <div>
+            <h3>  <img  src="./img/less.png" width="600px" alt="nothing"/></h3>
+          </div>
+          <div>
+          <h3>  <img  src="./img/less.png" width="600px" alt="nothing"/></h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
+      </div>
       </div>
     );
   }
