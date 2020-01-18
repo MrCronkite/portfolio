30ed1f8c103524ae2c84071a954be9b9c66c89a4
MrCronkite <vladshima99@gmail.com>
2020-01-18 22:25:16 +0300

    change gulpfile

--- repairs/gulpfile.js
+++ repairs/gulpfile.js
@@ -1,41 +1,60 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
-const autopref = require("gulp-autoprefixer");
+const autoprefixer = require("gulp-autoprefixer");
 const concat = require("gulp-concat");
 const imagemin = require("gulp-imagemin");
-const cleanCss = require("gulp-clean-css");
-const browsersync = require("browser-sync").create();
+const cleanCSS = require("gulp-clean-css");
+const browserSync = require("browser-sync").create();
+const del = require("del");
 
-styles = () => {
-  gulp
+function styles() {
+  return gulp
     .src("./src/styles/**/*.scss")
     .pipe(sass().on("error", sass.logError))
-    .pipe(concat("min.bundle.css"))
+    .pipe(concat("style.css"))
     .pipe(
-      autopref({
+      autoprefixer({
         cascade: false
       })
     )
-    .pipe(cleanCss({ compatibility: "ie8" }))
-    .pipe(gulp.dest("./dist/css/"))
-    .pipe(browsersync.stream());
-};
+    .pipe(
+      cleanCSS({
+        level: 2
+      })
+    )
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(browserSync.stream());
+}
 
-image = () => {
-  gulp
-    .src("src/img/*")
-    .pipe(imagemin())
-    .pipe(gulp.dest("./dist/imgs"))
-    .pipe(browsersync.stream());
-};
+function clean() {
+  return del(["dist/*"]);
+}
 
-gulp.task("watch", () => {
-  browsersync.init({
+function watch() {
+  browserSync.init({
     server: {
       baseDir: "./"
     }
   });
   gulp.watch("./src/styles/**/*.scss", styles);
   gulp.watch("./src/img/", image);
-  gulp.watch("./*.html").on("change", browsersync.reload);
-});
+  gulp.watch("./*.html").on("change", browserSync.reload);
+}
+
+function image() {
+  return gulp
+    .src("src/img/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./dist/imgs"))
+    .pipe(browserSync.stream());
+}
+
+gulp.task("styles", styles);
+
+gulp.task("del", clean);
+
+gulp.task("watch", watch);
+
+gulp.task("build", gulp.series(clean, gulp.parallel(styles, image)));
+
+gulp.task("dev", gulp.series("build", "watch"));
