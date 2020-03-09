9d21567b09058b6e02e75409cbe82202da314bb6
MrCronkite <vladshima99@gmail.com>
2020-03-09 23:40:16 +0300

    new site

---
+++ VladislavShimchenko/gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autopref = require("gulp-autoprefixer");
+const concat = require("gulp-concat");
+const cleanCss = require("gulp-clean-css");
+const browsersync = require("browser-sync").create();
+const del = require("del");
+
+function styles() {
+  return gulp
+    .src("./src/styles/scss/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(concat("all.css"))
+    .pipe(
+      autopref({
+        cascade: false
+      })
+    )
+    .pipe(cleanCss({ compatibility: "ie8" }))
+    .pipe(gulp.dest("./dist/css/"))
+    .pipe(browsersync.stream());
+}
+
+function clean() {
+  return del(["dist/css/"]);
+}
+
+function watch() {
+  browsersync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch("./src/styles/scss/*.scss", styles);
+  gulp.watch("./*.html").on("change", browsersync.reload);
+}
+
+gulp.task("watch", watch);
+
+gulp.task("build", gulp.series(clean, gulp.parallel(styles)));
+
+gulp.task("dev", gulp.series("build", "watch"));
